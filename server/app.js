16e78ef5ff5e7c47a5709271aba36b9829d7f3d5
UtibeAbasi <1.BarukhOr@gmail.com>
2016-06-22 19:46:00 -0400

    Small Steps
    + Simple Server
    - Attempting to run a test results in an error saying 'ReferenceError: describe is not defined'

---
+++ server/app.js
@@ -0,0 +1,19 @@
+//Starting point of the application 
+const bodyParser = require('body-parser');
+const express = require('express');
+const http = require('http');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const app = express();
+const router = require('./routes/index.js');
+
+//App Setup
+app.use(morgan('dev'));
+app.use(bodyParser.json({type:'*/*'}));
+router(app);
+
+//Server Setup
+const port = process.env.PORT || 3000;
+const server = http.createServer(app);
+server.listen(port,'0.0.0.0');
+console.log('Server listening on: ', port);
---
+++ test/test-server.js
@@ -0,0 +1,10 @@
+import chai, { expect } from 'chai';
+import chaiHttp from 'chai-http';
+import App from '../server/app.js';
+chai.use(chaiHttp);
+
+// use 'describe' to group together similar tests
+describe('App',function(){
+  // it('Should return a string')
+});
+
