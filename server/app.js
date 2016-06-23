d0616b6b3f08c85afae8d09702ec717a150d6d01
UtibeAbasi <1.BarukhOr@gmail.com>
2016-06-23 17:21:37 -0400

    Minor Bugfix
    + Necessary to export server so that it can be "tested"

--- server/app.js
+++ server/app.js
@@ -17,3 +17,4 @@ const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 server.listen(port,'0.0.0.0');
 console.log('Server listening on: ', port);
+module.exports = server;
