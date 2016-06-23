e5763319047157004d7da61aea170dd4f6c476c1
UtibeAbasi <1.BarukhOr@gmail.com>
2016-06-23 17:23:54 -0400

    Testing 1
    + Successfully runs
    - Returns invalid result ( Should error out as res.text should be equal to an array not a 'simple' string )

--- test/test-server.js
+++ test/test-server.js
@@ -1,10 +1,20 @@
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
-import App from '../server/app.js';
+import server from '../server/app.js';
 chai.use(chaiHttp);
 
 // use 'describe' to group together similar tests
 describe('App',function(){
-  // it('Should return a string')
+	it('should return an array of strings',() => {
+		chai.request(server)
+			.get('/test')
+			.end(function(err,res){
+				// console.log(res.text)
+				expect(res.text).to.be.eql("ok ... whats going on ");
+			})
+	})
+	it('should list all users on /users GET');
+	it('should list a single user on /user/<id> GET');;
+	it('should add a single user on /user POST');
 });
 
