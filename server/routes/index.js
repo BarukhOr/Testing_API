dc576a5e9f7a7d930a63da45e6d43eabcfabe307
UtibeAbasi <1.BarukhOr@gmail.com>
2016-06-26 09:58:45 -0400

    Testing 1
    + Now returns valid result

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,5 @@
 module.exports = function(app){
-  app.get('/test',function(request,response,next){
+  app.get('/',function(request,response,next){
     response.send(["Greetings welcome to the jungle","Phone","Stool","Speaker"])
   });
 }
--- test/test-server.js
+++ test/test-server.js
@@ -5,16 +5,16 @@ chai.use(chaiHttp);
 
 // use 'describe' to group together similar tests
 describe('App',function(){
-	it('should return an array of strings',() => {
+	it('testing that the app is up', (done)=> {
 		chai.request(server)
-			.get('/test')
-			.end(function(err,res){
-				// console.log(res.text)
-				expect(res.text).to.be.eql("ok ... whats going on ");
-			})
+			.get('/')
+			.end(function (err, res) {
+		     expect(err).to.be.null;
+		     expect(res).to.have.status(200);
+		     done();
+		  });
 	})
 	it('should list all users on /users GET');
 	it('should list a single user on /user/<id> GET');;
 	it('should add a single user on /user POST');
 });
-
