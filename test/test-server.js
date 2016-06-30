bd271c75453f4989630195d9fea4e0432ac7bde2
UtibeAbasi <1.BarukhOr@gmail.com>
2016-06-29 22:50:45 -0400

    Test Authentication Module
    + Implemented some simple tests to verify the functionality of the routes that are currently available

--- test/test-server.js
+++ test/test-server.js
@@ -1,11 +1,13 @@
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
-import server from '../server/app.js';
+import config from '../config.js';
+const server = config.server;
+
 chai.use(chaiHttp);
 
 // use 'describe' to group together similar tests
-describe('App',function(){
-	it('testing that the app is up', (done)=> {
+describe('Authentication',function(){
+	it('tests whether the app is up', (done)=> {
 		chai.request(server)
 			.get('/')
 			.end(function (err, res) {
@@ -14,7 +16,37 @@ describe('App',function(){
 		     done();
 		  });
 	})
-	it('should list all users on /users GET');
-	it('should list a single user on /user/<id> GET');;
-	it('should add a single user on /user POST');
+
+	it('tests whether POST /signup works',(done)=> {
+		chai.request(server)
+			.post('/signup')
+			.send({'email': 'Java@Script.io', 'password': '123456'})
+			.end(function(err,res){
+				expect(err).to.be.null;
+				expect(res).to.have.status(200);
+				expect(res.body).to.have.property('token');
+				done();
+			})
+	})
+
+	it('tests whether POST /signup works with only a password',(done)=> {
+		chai.request(server)
+			.post('/signup')
+			.send({'password': '123456'})
+			.end(function(err,res){
+				expect(res).to.have.status(422);
+				done();
+			})
+	})
+	
+	it('should delete the user account created in test /signup',(done)=>{
+		chai.request(server)
+			.post('/deleteUser')
+			.send({'email': 'Java@Script.io'})
+			.end(function(err,res){
+				expect(err).to.be.null;
+				expect(res).to.have.status(204);
+				done();
+			})
+	});
 });
