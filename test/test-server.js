ee71a452da5def53b8a5d3f47221d9b030bfa6cd
UtibeAbasi <1.BarukhOr@gmail.com>
2016-07-01 05:54:22 -0400

    Testing
    + Changed Structure of the tests
    + Verifies that the route checks for a valid token
    + Verifies that invalid token results in failed query

--- test/test-server.js
+++ test/test-server.js
@@ -6,47 +6,60 @@ const server = config.server;
 chai.use(chaiHttp);
 
 // use 'describe' to group together similar tests
-describe('Authentication',function(){
-	it('tests whether the app is up', (done)=> {
-		chai.request(server)
-			.get('/')
-			.end(function (err, res) {
-		     expect(err).to.be.null;
-		     expect(res).to.have.status(200);
-		     done();
-		  });
-	})
+describe('User',function(){
+	describe("Validation", function(){
 
-	it('tests whether POST /signup works',(done)=> {
-		chai.request(server)
-			.post('/signup')
-			.send({'email': 'Java@Script.io', 'password': '123456'})
-			.end(function(err,res){
-				expect(err).to.be.null;
-				expect(res).to.have.status(200);
-				expect(res.body).to.have.property('token');
-				done();
-			})
-	})
+		it('tests whether POST /signup works',(done)=> {
+			chai.request(server)
+				.post('/signup')
+				.send({'email': 'Java@Script.io', 'password': '123456'})
+				.end(function(err,res){
+					expect(err).to.be.null;
+					expect(res).to.have.status(200);
+					expect(res.body).to.have.property('token');
+					done();
+				})
+		})
 
-	it('tests whether POST /signup works with only a password',(done)=> {
-		chai.request(server)
-			.post('/signup')
-			.send({'password': '123456'})
-			.end(function(err,res){
-				expect(res).to.have.status(422);
-				done();
-			})
+		it('tests whether POST /signup works with only a password',(done)=> {
+			chai.request(server)
+				.post('/signup')
+				.send({'password': '123456'})
+				.end(function(err,res){
+					expect(res).to.have.status(422);
+					done();
+				})
+		})
+		
+		it('should delete the user account created in test /signup',(done)=>{
+			chai.request(server)
+				.post('/deleteUser')
+				.send({'email': 'Java@Script.io'})
+				.end(function(err,res){
+					expect(err).to.be.null;
+					expect(res).to.have.status(204);
+					done();
+				})
+		});
+
+		it('gets a status 200 from the authenticated test route with a valid token',(done)=>{
+			chai.request(server)
+				.get('/authenticatedtest')
+				.set("authorization",'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI1Nzc2MGMyOTVjYTBjMzg0MjEzMzBkZTUiLCJpYXQiOjE0NjczNTQxNTQzMDYsImV4cCI6MTQ2NzM3NTc1NDMwNn0.CX9TskZvbK5ZXrc2lfscK63hIIvY1pZ_zA_qetmF0QE')
+				.end(function(err,res){
+					expect(res).to.have.status(200);
+					done();
+				})
+		});
+
+		it('fails, when making a request to the authenticated route using an improper token',(done)=>{
+			chai.request(server)
+				.get('/authenticatedtest')
+				.set("authorization",'eyJ0eXAiOiJKV1QiLCiJIUzI1NiJ9.eyJzdWIiOiI1Nzc2MGMyOTVjYTBjMzg0MjEzMzBkZTUiLCJpYXQiOjE0NjczNTQxNTQzMDYsImV4cCI6MTQ2NzM3NTc1NDMwNn0.CX9TskZvbK5ZXrc2lfscK63hIIvY1pZ_zA_qetmF0QE')
+				.end(function(err,res){
+					expect(res).to.have.status(401);
+					done();
+				})
+		});
 	})
-	
-	it('should delete the user account created in test /signup',(done)=>{
-		chai.request(server)
-			.post('/deleteUser')
-			.send({'email': 'Java@Script.io'})
-			.end(function(err,res){
-				expect(err).to.be.null;
-				expect(res).to.have.status(204);
-				done();
-			})
-	});
 });
